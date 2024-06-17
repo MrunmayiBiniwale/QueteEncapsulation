f1685e8389beaf279a597e8df434cd08b8a5b07f
Mrunmayi Biniwale <mrunmayibiniwale99@gmail.com>
2024-06-17 14:01:28 +0200

    Ajoutez des fichiers projet.

---
+++ BeerEncapsulator.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace QueteEncapsulation
+{
+    public class BeerEncapsulator
+    {
+        const decimal eachAddBeerVolume = 33;
+        const int requiredBottleAndCapsuleEachBeer = 1;
+        private decimal _availableBeerVolume;
+        private int _availableBottles;
+        private int _availableCapsules;
+        public int beerBottlesRequiredByUser = 0;
+        public int nombreDeBouteillePeutFabriquer = 0;
+
+        //Get user input
+        public void GetUserData()
+        {
+            Console.WriteLine("Please enter the available quantity of beer in litres : ");
+            decimal.TryParse(Console.ReadLine(), out _availableBeerVolume);
+            Console.WriteLine("Please enter the available number of bottles : ");
+            int.TryParse(Console.ReadLine(), out _availableBottles);
+            Console.WriteLine("Please enter the available number of capsules : ");
+            int.TryParse(Console.ReadLine(), out _availableCapsules);
+        }
+
+        //Validate if all elements are available and add beer
+        public void AddBeer(int beerBottlesRequiredByUser)
+        {           
+            if (ValidateElements(beerBottlesRequiredByUser))
+            {
+                nombreDeBouteillePeutFabriquer = ProduceEncapsulatedBeerBottles(beerBottlesRequiredByUser);
+                Console.WriteLine("\n" + nombreDeBouteillePeutFabriquer + " number of bottles can be fabricated.");
+                if (nombreDeBouteillePeutFabriquer > beerBottlesRequiredByUser)
+                {
+                    _availableBeerVolume -= (eachAddBeerVolume * beerBottlesRequiredByUser);
+                    _availableBottles -= (requiredBottleAndCapsuleEachBeer * beerBottlesRequiredByUser);
+                    _availableCapsules -= (requiredBottleAndCapsuleEachBeer * beerBottlesRequiredByUser);
+                    Console.WriteLine("" + beerBottlesRequiredByUser + " are fabricated as requested.");
+                }                
+            }              
+        }
+
+        //Check how many beer bottles can be fabricated
+        public int ProduceEncapsulatedBeerBottles(int nombreDeBouteilleDoitFrabiquer = 0)
+        {
+            if (_availableBeerVolume >= (eachAddBeerVolume * nombreDeBouteilleDoitFrabiquer)
+                && _availableBottles >= (requiredBottleAndCapsuleEachBeer * nombreDeBouteilleDoitFrabiquer)
+                && _availableBottles >= (requiredBottleAndCapsuleEachBeer * nombreDeBouteilleDoitFrabiquer))
+            {
+                if((_availableBottles * eachAddBeerVolume) <= _availableBeerVolume)
+                {
+                    nombreDeBouteillePeutFabriquer =  _availableBottles < _availableCapsules ? _availableBottles : _availableCapsules;
+                }
+                else
+                {
+                    for (int i = _availableBottles; i > 0; i--)
+                    {
+                        if (_availableBeerVolume >= (eachAddBeerVolume * (i))
+                            && _availableBottles >= (requiredBottleAndCapsuleEachBeer * (i))
+                            && _availableBottles >= (requiredBottleAndCapsuleEachBeer * (i)))
+                        {
+                            nombreDeBouteillePeutFabriquer = i;
+                            break;
+                        }
+                    }
+                }               
+            }
+            else
+            {
+                int nombreDeBouteillePeutFabriquer = 0;
+                for(int i = nombreDeBouteilleDoitFrabiquer; i > 0; i--)
+                {
+                    if (_availableBeerVolume >= (eachAddBeerVolume * (i))
+                        && _availableBottles >= (requiredBottleAndCapsuleEachBeer * (i))
+                        && _availableBottles >= (requiredBottleAndCapsuleEachBeer * (i)))
+                    {
+                        nombreDeBouteillePeutFabriquer = i;
+                        break;
+                    }                        
+                }  
+            }            
+            return nombreDeBouteillePeutFabriquer;
+        }
+
+        //Validate if all elements are available
+        public bool ValidateElements(int beerBottlesRequiredByUser)
+        {
+            bool isValide;
+            if (_availableBeerVolume < (eachAddBeerVolume * beerBottlesRequiredByUser))
+            {
+                isValide = false;
+                decimal requiredBeerVolume = (eachAddBeerVolume * beerBottlesRequiredByUser) - _availableBeerVolume;
+                Console.WriteLine("Not enough beer available. Please add " + requiredBeerVolume + " litres of beer.");
+            }      
+            else if(_availableBottles < (requiredBottleAndCapsuleEachBeer * beerBottlesRequiredByUser))
+            {
+                isValide = false;
+                int requiredBeerBottles = (requiredBottleAndCapsuleEachBeer * beerBottlesRequiredByUser) - _availableBottles;
+                Console.WriteLine("Not enough bottles available. Please add " + requiredBeerBottles + " bottles.");
+            }
+            else if (_availableCapsules < (requiredBottleAndCapsuleEachBeer * beerBottlesRequiredByUser))
+            {
+                isValide = false;
+                int requiredCapsules = (requiredBottleAndCapsuleEachBeer * beerBottlesRequiredByUser) - _availableCapsules;
+                Console.WriteLine("Not enough capsules available. Please add " + requiredCapsules + " capsules.");
+            }
+            else
+            {
+                isValide = true;
+            }
+
+            return isValide;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace QueteEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int isAvailableQuantityEnoughToContinue = 0;
+            BeerEncapsulator beer = new BeerEncapsulator();
+            beer.GetUserData();
+            while (true)
+            {
+                Console.WriteLine("Enter the bumber of bottles required : ");
+                int.TryParse(Console.ReadLine(), out beer.beerBottlesRequiredByUser);
+                beer.AddBeer(beer.beerBottlesRequiredByUser);
+                isAvailableQuantityEnoughToContinue = beer.ProduceEncapsulatedBeerBottles();
+                if (isAvailableQuantityEnoughToContinue > 0)
+                {
+                    Console.WriteLine("Do you want to continue fabricating bottles? y/n");
+                    if (Console.ReadLine().ToLower() != "y")
+                        break;
+                }
+                else
+                    break;
+            }            
+        }
+    }
+}
